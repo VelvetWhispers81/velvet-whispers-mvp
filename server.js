3b712e690b1cbc83ccd32ab76c762c629ddd24e7
VelvetWhispers81 <mummabull81@gmail.com>
2026-06-19 11:38:35 -0500

    Convert server.js to use ES6 imports
    Refactor server.js to use ES6 import syntax.

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(cors());
+app.use(express.json());
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
