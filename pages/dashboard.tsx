bccfbacfeed9b970b19653b9b40e4e21fd558453
VelvetWhispers81 <mummabull81@gmail.com>
2026-06-30 04:25:20 -0500

    Delete pages/dashboard.tsx

--- pages/dashboard.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Dashboard</h1>
-      <p>Your Velvet Whispers dashboard is now active.</p>
-    </div>
-  );
-}pages/dashboard.tsx
